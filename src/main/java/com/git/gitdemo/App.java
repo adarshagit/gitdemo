f9881c1bdc6ae67acb0832a5529aa3f873d3f2ab
adarshagit <adarshakr.only1@gmail.com>
2018-11-26 18:59:10 +0530

    Ceating CONant class for not hard coding

--- src/main/java/com/git/gitdemo/App.java
+++ src/main/java/com/git/gitdemo/App.java
@@ -9,7 +9,7 @@ public class App
 	
 	public void addName(String name) {
 		System.out.println("adarsha");
-		if(name.equals("Adarsha")) {
+		if(name.equals(Constant.DATA_TYPE)) {
 			System.out.println("welcome Adarsha");
 		}else {
 			System.out.println("welcome Ram bhaiaya ti saanapur");
---
+++ src/main/java/com/git/gitdemo/Constant.java
@@ -0,0 +1,8 @@
+package com.git.gitdemo;
+
+public class Constant {
+
+	
+	
+public static String DATA_TYPE="java Techie";
+}
