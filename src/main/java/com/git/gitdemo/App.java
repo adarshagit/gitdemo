ec7e0d26455a993dc96ff5335c7658f150895038
adarshagit <adarshakr.only1@gmail.com>
2018-11-26 14:38:23 +0530

    msg saved

---
+++ src/main/java/com/git/gitdemo/App.java
@@ -0,0 +1,22 @@
+package com.git.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void addName(String name) {
+		System.out.println("adarsha");
+		if(name.equals("Adarsha")) {
+			System.out.println("welcome Adarsha");
+		}else {
+			System.out.println("welcome Ram bhaiaya");
+		}
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "First git demo" );
+    }
+}
